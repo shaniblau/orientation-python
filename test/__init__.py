ae52aa1c4f6214a2114c5d0a111b559e9bb6cadc
שני בלאו <shaniblau@snys-MacBook-Air.local>
2023-04-30 17:44:08 +0300

    check

--- test/__init__.py
+++ test/__init__.py
@@ -1,3 +1,3 @@
-from .test_end_to_end import SanityTest
-from .test_csv_extract import CSVExtractTest
-from .test_json_load import JsonLoadTest
+from . import test_end_to_end
+from . import test_csv_extract
+from . import test_json_load
