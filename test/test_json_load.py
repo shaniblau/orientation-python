cb8bfda1393fc9d5be7162f371c3e87dc881f235
שני בלאו <shaniblau@snys-MacBook-Air.local>
2023-04-30 13:50:54 +0300

    added the json loader tests

---
+++ test/test_json_load.py
@@ -0,0 +1,30 @@
+import unittest
+
+from load import JsonLoad
+
+
+class TestJsonLoad(unittest.TestCase):
+    data = []
+    for i in range(100000):
+        data.append(i)
+    with open("/Users/shaniblau/Documents/pythonProjects/new/fadw", 'r') as file:
+        lines = file.read()
+    items = lines.split("/n")[:-1]
+
+    def test_create_sub_list_func(self):
+        self.assertEqual(JsonLoad.create_sub_list(self.data, 0, 3), [0, 1, 2, 3])
+
+    def test_hermeticity(self):
+        amount = len(self.items)
+        self.assertEqual(amount, 100000)
+
+    def test_reliability(self):
+        self.assertTrue(self.check_equals())
+
+    def check_equals(self):
+        for item in self.items:
+            if int(item) not in self.data:
+                return False
+        return True
+
+
