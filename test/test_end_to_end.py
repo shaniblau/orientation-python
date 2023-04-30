dd219ca00fb33d189251ea48212d6d8c80ec4a85
שני בלאו <shaniblau@snys-MacBook-Air.local>
2023-04-30 17:19:15 +0300

    check

--- test/test_end_to_end.py
+++ test/test_end_to_end.py
@@ -3,7 +3,7 @@ import unittest
 
 
 class Sanity(unittest.TestCase):
-    path = '/Users/shaniblau/Documents/pythonProjects/orientation-python/mada_reports'
+    path = '../mada_reports'
 
     def test_sanity(self):
         self.assertTrue(os.path.exists(self.path))
