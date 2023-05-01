dd5d46f68342ca49e6e337f981350fad6670a63b
שני בלאו <shaniblau@snys-MacBook-Air.local>
2023-05-01 09:56:11 +0300

    check

--- test/csv_extract_test.py
+++ test/csv_extract_test.py
@@ -1,41 +1,45 @@
 import csv
 from extract import csv_extract
 
-path: str = "../MadaReports - MadaReports.csv"
-data: list = csv_extract()
 
+class CSVExtractTest:
+    path: str = "../MadaReports - MadaReports.csv"
+    data: list = csv_extract()
 
-def check_equals(a_path):
-    i = 0
-    with open(a_path, 'r') as file:
-        reader = csv.DictReader(file)
-        for row in reader:
-            if row not in data:
-                i += 1
-                if i > 1:
-                    return False
-    return True
-
-
-def check_error():
-    try:
-        csv_extract("path")
-    except FileNotFoundError:
+    def check_equals(self, path):
+        i = 0
+        with open(path, 'r') as file:
+            reader = csv.DictReader(file)
+            for row in reader:
+                if row not in self.data:
+                    i += 1
+                    if i > 1:
+                        return False
         return True
-    return False
+
+    @staticmethod
+    def check_error():
+        try:
+            csv_extract("path")
+        except FileNotFoundError:
+            return True
+        return False
+
+
+cet = CSVExtractTest()
 
 
 def test_extract_func():
-    assert data != []
+    assert cet.data != []
 
 
 def test_hermeticity():
-    assert len(data) == 1000
+    assert len(cet.data) == 1000
 
 
 def test_reliability():
-    assert check_equals(path) == True
+    assert cet.check_equals(cet.path) == True
 
 
 def test_file_not_exists():
-    assert check_error() == True
+    assert cet.check_error() == True
