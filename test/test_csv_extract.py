3a0d8a84f6a4e8db489195fa3f69b949771ef2df
שני בלאו <shaniblau@snys-MacBook-Air.local>
2023-05-01 13:28:41 +0300

    check

--- test/test_csv_extract.py
+++ test/test_csv_extract.py
@@ -13,14 +13,6 @@ def check_equals(csv_extract_fixture, path: str = "../MadaReports - MadaReports.
     return True
 
 
-def check_error(csv_extract_fixture):
-    try:
-        csv_extract_fixture("path")
-    except FileNotFoundError:
-        return True
-    return False
-
-
 def test_extract_func(csv_extract_fixture):
     assert csv_extract_fixture() != []
 
@@ -32,6 +24,3 @@ def test_hermeticity(csv_extract_fixture):
 def test_reliability(csv_extract_fixture):
     assert check_equals(csv_extract_fixture()) == True
 
-
-def test_file_not_exists(csv_extract_fixture):
-    assert check_error(csv_extract_fixture) == True
